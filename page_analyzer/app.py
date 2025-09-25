49dc88c752ac7fc0bd9af30566962aa38eb2c1e4
Paul Nozdrachev <p.nozdra4ev@yandex.ru>
2025-09-25 13:07:35 +0300

    fix: remove application factory

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -6,7 +6,6 @@ import validators
 from dotenv import load_dotenv
 from page_analyzer.urls import UrlsRepository
 from page_analyzer.urls import URLChecksRepository
-from page_analyzer.urls import init_pool
 from flask import (
     Flask,
     render_template,
@@ -20,67 +19,63 @@ from flask import (
 
 load_dotenv()
 
-
-def app():
-    application = Flask(__name__)
-    application.config["SECRET_KEY"] = secrets.token_hex(16)
-    dsn = os.getenv("DATABASE_URL")
-    conn_pool = init_pool(dsn)
-    urls_repository = UrlsRepository(conn_pool)
-    url_check_repository = URLChecksRepository(conn_pool)
+app = Flask(__name__)
+app.config["SECRET_KEY"] = secrets.token_hex(16)
+dsn = os.getenv("DATABASE_URL")
+urls_repository = UrlsRepository(dsn)
+url_check_repository = URLChecksRepository(dsn)
 
 
-    @application.route("/")
-    def index():
-        messages = get_flashed_messages(
-            with_categories=True)
+@app.route("/")
+def index():
+    messages = get_flashed_messages(
+        with_categories=True)
+    print(messages)
+    return render_template(
+        "index.html",
+        messages=messages)
+
+
+@app.get("/urls")
+def urls_index():
+    sites = urls_repository.index()
+    return render_template(
+        "urls.html",
+        sites=sites
+    )
+
+
+@app.get("/urls/<id>")
+def url_show(id):
+    url_info = urls_repository.find(id)
+    url_checks = url_check_repository.index(id)
+    if url_info:
         return render_template(
-            "index.html",
-            messages=messages)
+            "url_info.html",
+            site=url_info,
+            checks=url_checks)
 
 
-    @application.get("/urls")
-    def urls_index():
-        sites = urls_repository.index()
-
-        return render_template(
-            "urls.html",
-            sites=sites
-        )
-
-    @application.get("/urls/<id>")
-    def url_show(id):
-        url_info = urls_repository.find(id)
-        url_checks = url_check_repository.index(id)
-        if url_info:
-            return render_template(
-                "url_info.html",
-                site=url_info,
-                checks=url_checks)
-
-
-    @application.post("/urls")
-    def urls():
-        form_data = request.form.to_dict()
-        url_string = form_data.get("url")
-        err_msg = validate_url(url_string)
-        if err_msg:
-            flash(err_msg, category="error")
-            return redirect(url_for("index"))
-        parsed_url = urlparse(url_string)
-        status, msg = urls_repository.save(
-            f"{parsed_url.scheme}://{parsed_url.hostname}",
-            datetime.datetime.now())
-        flash(msg, status)
+@app.post("/urls")
+def urls():
+    form_data = request.form.to_dict()
+    url_string = form_data.get("url")
+    err_msg = validate_url(url_string)
+    if err_msg:
+        flash(err_msg, category="error")
         return redirect(url_for("index"))
+    parsed_url = urlparse(url_string)
+    status, msg = urls_repository.save(
+        f"{parsed_url.scheme}://{parsed_url.hostname}",
+        datetime.datetime.now())
+    flash(msg, status)
+    return redirect(url_for("index"))
 
 
-    @application.post("/urls/<id>/checks")
-    def create_new_check(id):
-        url_check_repository.save(id)
-        return redirect(url_for("url_show", id=id))
-
-    return application
+@app.post("/urls/<id>/checks")
+def create_new_check(id):
+    url_check_repository.save(id)
+    return redirect(url_for("url_show", id=id))
 
 
 def validate_url(url_string: str) -> str | None:
@@ -89,4 +84,3 @@ def validate_url(url_string: str) -> str | None:
     if validators.url(url_string) is not True:
         return "Invalid URL format"
     return None
-
--- page_analyzer/urls.py
+++ page_analyzer/urls.py
@@ -9,126 +9,107 @@ def init_pool(dsn: str) -> pool.SimpleConnectionPool:
 
 
 class UrlsRepository:
-    def __init__(self, conn_pool: pool.SimpleConnectionPool):
-        self.conn_pool = conn_pool
+    def __init__(self, dsn: str):
+        self.dsn = dsn
+
+    def _connect_to_db(self):
+        return psycopg2.connect(self.dsn)
 
     def save(self, url: str, created_at) -> tuple:
-        db_connection = self.conn_pool.getconn()
         message = tuple()
-        try:
-            if db_connection:
-                with db_connection.cursor() as cursor:
-                    try:
-                        sql_query = "INSERT INTO urls (name, created_at) VALUES (%s, %s);"
-                        cursor.execute(sql_query, (url, created_at))
-                        db_connection.commit()
-                        message = ("success", "URL added successfully")
-                    except psycopg2.errors.UniqueViolation:
-                        message = ("error", "URL already exists")
-                        db_connection.rollback()
-            return message
-        finally:
-            self.conn_pool.putconn(db_connection)
+        with self._connect_to_db() as db_connection:
+            with db_connection.cursor() as cursor:
+                try:
+                    sql_query = "INSERT INTO urls (name, created_at) VALUES (%s, %s);"
+                    cursor.execute(sql_query, (url, created_at))
+                    db_connection.commit()
+                    message = ("success", "URL added successfully")
+                except psycopg2.errors.UniqueViolation:
+                    message = ("error", "URL already exists")
+                    db_connection.rollback()
+        return message
 
     def index(self) -> list[dict]:
-        db_connection = self.conn_pool.getconn()
         results = []
-        try:
-            if db_connection:
-                with db_connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
-                    sql_query = """
-                    select
-                        urls.id,
-                        name,
-                        url_checks.status_code,
-                        max(url_checks.created_at) as last_check
-                    from urls
-                    left join url_checks on urls.id = url_checks.url_id
-                    group by urls.id, name, status_code
-                    order by urls.id desc"""
-                    cursor.execute(sql_query)
-                    rows = cursor.fetchall()
-                    for row in rows:
-                        url_info = {
-                            "id": row.id,
-                            "name": row.name,
-                            "status_code": row.status_code if row.status_code is not None else "",
-                            "last_check": row.last_check.date() if row.last_check is not None else ""
-                        }
-                        results.append(url_info)
-        finally:
-            self.conn_pool.putconn(db_connection)
+        with self._connect_to_db() as db_connection:
+            with db_connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
+                sql_query = """
+                            select urls.id, \
+                                   name, \
+                                   url_checks.status_code, \
+                                   max(url_checks.created_at) as last_check
+                            from urls
+                                     left join url_checks on urls.id = url_checks.url_id
+                            group by urls.id, name, status_code
+                            order by urls.id desc"""
+                cursor.execute(sql_query)
+                rows = cursor.fetchall()
+                for row in rows:
+                    url_info = {
+                        "id": row.id,
+                        "name": row.name,
+                        "status_code": row.status_code if row.status_code is not None else "",
+                        "last_check": row.last_check.date() if row.last_check is not None else ""
+                    }
+                    results.append(url_info)
         return results
 
     def find(self, url_id) -> dict:
-        db_connection = self.conn_pool.getconn()
         result = {}
-        try:
-            if db_connection:
-                with db_connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
-                    sql_query = "SELECT id, name, created_at FROM urls WHERE id = %s;"
-                    cursor.execute(sql_query, (url_id,))
-                    row = cursor.fetchone()
-                    result["id"] = row.id
-                    result["name"] = row.name
-                    result["created_at"] = row.created_at.date()
-            return result
-        finally:
-            self.conn_pool.putconn(db_connection)
+        with self._connect_to_db() as db_connection:
+            with db_connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
+                sql_query = "SELECT id, name, created_at FROM urls WHERE id = %s;"
+                cursor.execute(sql_query, (url_id,))
+                row = cursor.fetchone()
+                result["id"] = row.id
+                result["name"] = row.name
+                result["created_at"] = row.created_at.date()
+        return result
 
 
 class URLChecksRepository:
-    def __init__(self, conn_pool: pool.SimpleConnectionPool):
-        self.conn_pool = conn_pool
+    def __init__(self, dsn: str):
+        self.dsn = dsn
+
+    def _connect_to_db(self):
+        return psycopg2.connect(self.dsn)
 
     def save(self, url_id: int):
-        db_connection = self.conn_pool.getconn()
-        try:
-            if db_connection:
-                sql_query = "INSERT into url_checks(url_id, created_at) VALUES (%s, %s);"
-                with db_connection.cursor() as cursor:
-                    cursor.execute(sql_query, (url_id, datetime.now()))
-                db_connection.commit()
-        finally:
-            self.conn_pool.putconn(db_connection)
+        with self._connect_to_db() as db_connection:
+            sql_query = "INSERT into url_checks(url_id, created_at) VALUES (%s, %s);"
+            with db_connection.cursor() as cursor:
+                cursor.execute(sql_query, (url_id, datetime.now()))
+            db_connection.commit()
 
     def index(self, url_id) -> list[dict]:
-        db_connection = self.conn_pool.getconn()
         results = []
-        try:
-            if db_connection:
-                sql_query = "SELECT id, url_id, status_code, h1, title, description, created_at FROM url_checks WHERE url_id=%s;"
-                with db_connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
-                    cursor.execute(sql_query, (url_id,))
-                    rows = cursor.fetchall()
-                    for row in rows:
-                        check = {
-                            "id": row.id,
-                            "url_id": row.url_id,
-                            "status_code": row.status_code if row.status_code is not None else "",
-                            "h1": row.h1 if row.h1 is not None else "",
-                            "title": row.title if row.title is not None else "",
-                            "description": row.description if row.description is not None else "",
-                            "created_at": row.created_at.date()
-                        }
-                        results.append(check)
-            return results
-        finally:
-            self.conn_pool.putconn(db_connection)
-
-    def get_last_check_info(self, url_id) -> dict | None:
-        db_connection = self.conn_pool.getconn()
-        last_check_info = None
-        try:
-            if db_connection:
-                sql_query = "SELECT status_code, created_at FROM url_checks WHERE url_id=%s ORDER BY created_at DESC limit 1;"
-                with db_connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
-                    cursor.execute(sql_query, (url_id,))
-                    row = cursor.fetchone()
-                    last_check_info = {
+        with self._connect_to_db() as db_connection:
+            sql_query = "SELECT id, url_id, status_code, h1, title, description, created_at FROM url_checks WHERE url_id=%s;"
+            with db_connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
+                cursor.execute(sql_query, (url_id,))
+                rows = cursor.fetchall()
+                for row in rows:
+                    check = {
+                        "id": row.id,
+                        "url_id": row.url_id,
                         "status_code": row.status_code if row.status_code is not None else "",
+                        "h1": row.h1 if row.h1 is not None else "",
+                        "title": row.title if row.title is not None else "",
+                        "description": row.description if row.description is not None else "",
                         "created_at": row.created_at.date()
                     }
-        finally:
-            self.conn_pool.putconn(db_connection)
+                    results.append(check)
+        return results
+
+    def get_last_check_info(self, url_id) -> dict | None:
+        last_check_info = None
+        with self._connect_to_db() as db_connection:
+            sql_query = "SELECT status_code, created_at FROM url_checks WHERE url_id=%s ORDER BY created_at DESC limit 1;"
+            with db_connection.cursor(cursor_factory=NamedTupleCursor) as cursor:
+                cursor.execute(sql_query, (url_id,))
+                row = cursor.fetchone()
+                last_check_info = {
+                    "status_code": row.status_code if row.status_code is not None else "",
+                    "created_at": row.created_at.date()
+                }
         return last_check_info
